71512f14787efb81bed737a66f50acfa08f86a9a
ConorMcCarthy1 <90519422+ConorMcCarthy1@users.noreply.github.com>
2023-07-15 16:53:29 +0100

    Stored reviews in the context

--- movies/src/components/reviewForm/index.js
+++ movies/src/components/reviewForm/index.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState , useContext } from "react"; 
+import { MoviesContext } from "../../contexts/moviesContext";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form"; 
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useNavigate } from "react-router-dom";
+
+
+const context = useContext(MoviesContext);
 
 const ratings = [
   {
@@ -27,7 +34,11 @@ const ratings = [
     value: 0,
     label: "Terrible",
   },
-];
+]; 
+
+const [open, setOpen] = useState(false); 
+const navigate = useNavigate();
+
 
 const styles = {
   root: {
@@ -80,14 +91,38 @@ const ReviewForm = ({ movie }) => {
   const onSubmit = (review) => {
     review.movieId = movie.id;
     review.rating = rating;
-    console.log(review);
+    context.addReview(movie, review); 
+    setOpen(true); // NEW
   };
+
+  const handleSnackClose = (event) => {
+    setOpen(false);
+    navigate("/movies/favorites");
+  };
+
 
   return (
     <Box component="div" sx={styles.root}>
       <Typography component="h2" variant="h3">
         Write a review
       </Typography>
+
+      <Snackbar
+        sx={styles.snack}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={open}
+        onClose={handleSnackClose}
+      >
+        <MuiAlert
+          severity="success"
+          variant="filled"
+          onClose={handleSnackClose}
+        >
+          <Typography variant="h4">
+            Thank you for submitting a review
+          </Typography>
+        </MuiAlert>
+      </Snackbar>
 
       <form sx={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
         <Controller
